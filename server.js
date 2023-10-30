45248f9ba57c338060f65ea1c2c7b4e9a0c6179e
Sahan Randika <sahanr@infyinnos.com>
2023-10-30 21:45:32 +0530

    port

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT ||5000;
 
 app.use("/chat/api/chat",chatRoutes);
 app.use("/chat/api/message",messageRoutes); 
