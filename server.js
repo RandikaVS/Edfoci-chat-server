bfa27d34c59089acfd7e391f921ce22f2dbabea5
Sahan Randika <sahanr@infyinnos.com>
2024-07-24 12:05:44 +0530

    restart server

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-const PORT = process.env.PORT ||5000;
+const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
